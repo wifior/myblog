c4f96731c4308f2b473a00de78d460180b41175c
葛会军 <bgenius@sina.com>
2020-08-16 16:47:06 +0800

    博客发布

--- src/main/java/com/ghj/core/handler/MyMetaObjectHandler.java
+++ src/main/java/com/ghj/core/handler/MyMetaObjectHandler.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -19,12 +20,12 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     //insertFill在mp执行添加操作时运行
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.setFieldValByName("createTime",new Date(),metaObject);
-        this.setFieldValByName("updateTime",new Date(),metaObject);
+        this.setFieldValByName("createTime", LocalDateTime.now(),metaObject);
+        this.setFieldValByName("updateTime",LocalDateTime.now(),metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("updateTime",new Date(),metaObject);
+        this.setFieldValByName("updateTime",LocalDateTime.now(),metaObject);
     }
 }
--- src/main/java/com/ghj/web/myblog/controller/BlogController.java
+++ src/main/java/com/ghj/web/myblog/controller/BlogController.java
@@ -5,15 +5,15 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ghj.web.myblog.entity.Blog;
 import com.ghj.web.myblog.entity.vo.BlogVo;
 import com.ghj.web.myblog.service.BlogService;
+import com.ghj.web.myblog.service.TagService;
 import com.ghj.web.myblog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -31,6 +31,9 @@ public class BlogController {
     private BlogService blogService;
 
     @Autowired
+    private TagService tagService;
+
+    @Autowired
     private TypeService typeService;
 
     @GetMapping("/blogs")
@@ -41,11 +44,30 @@ public class BlogController {
         return "admin/blogs";
     }
 
-    @GetMapping("/blogs/search")
+    @PostMapping("/blogs/search")
     public String search(@RequestParam(value = "pageSize", defaultValue = "5",required = false) int pageSize, @RequestParam(value = "pageNum",defaultValue = "1",required = false) int pageNum, BlogVo blog, Model model){
         Page page = new Page(pageNum,pageSize);
         model.addAttribute("page",blogService.listBlog(page,blog));
         return "admin/blogs :: blogList";
+    }
+
+    @GetMapping("/blogs/input")
+    public String input(Model model){
+        model.addAttribute("types",typeService.listType());
+        model.addAttribute("tags",tagService.list());
+        model.addAttribute("blog",new Blog());
+        return "admin/blogsInput";
+    }
+
+    @PostMapping("/blogs")
+    public String post(Blog blog, RedirectAttributes attributes, HttpSession session){
+        boolean b = blogService.save(blog);
+        if(b){
+            attributes.addFlashAttribute("message","操作成功");
+        }else {
+            attributes.addFlashAttribute("message","操作失败");
+        }
+        return "redirect:/admin/blogs";
     }
 }
 
