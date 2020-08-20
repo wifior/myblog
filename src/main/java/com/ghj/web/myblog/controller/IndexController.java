1b6948690b9f400b4b8e92e769ed37973d5dc5c1
葛会军 <bgenius@sina.com>
2020-08-20 21:40:36 +0800

    博客前端展示markdown转html

--- src/main/java/com/ghj/web/myblog/controller/IndexController.java
+++ src/main/java/com/ghj/web/myblog/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,5 +47,11 @@ public class IndexController {
         model.addAttribute("page",blogService.listBlog(page,null));
         model.addAttribute("query",query);
         return "search";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blog(@PathVariable Long id, Model model){
+        model.addAttribute("blog",blogService.getBlog(id));
+        return "blog";
     }
 }
